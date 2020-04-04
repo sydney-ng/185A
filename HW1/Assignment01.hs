947cc38297a10d54a3d9879ccde5e2292a273a24
Sydney <sydney.ng@yahoo.com>
2020-04-03 20:53:04 -0700

    removed class ex

--- HW1/Assignment01.hs
+++ HW1/Assignment01.hs
@@ -12,15 +12,6 @@ denotation :: WFF -> Bool
 denotation f = case f of 
 	T -> True 
 	F -> False
-	Neg x -> denotation (not x)
+	Neg x -> denotation (T)
 --	Conj x y -> (denotation T) (denotation F)
 --	Disj x y -> Disj (denotation T) (denotation F)
-
-
-removeNegs :: WFF -> WFF
-removeNegs f = case f of
-	T -> T
-	F -> F
-	Neg x -> removeNegs x
-	Conj x y -> Conj (removeNegs x) (removeNegs y)
-	Disj x y -> Disj (removeNegs x) (removeNegs y)

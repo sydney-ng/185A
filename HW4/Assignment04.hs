d629379e6ae1c7cd07f8b7c66fa8028226278f5b
Sydney <sydney.ng@yahoo.com>
2020-04-28 14:47:46 -0700

    added hw4

--- HW4/Assignment04.hs
+++ HW4/Assignment04.hs
@@ -14,8 +14,8 @@ fsa2 :: Automaton Int SegmentCV
 fsa2 = ( 1, 
          [3],
          [(1, V, 1),
-         (2, V, 2),
          (1, C, 2),
+         (2, V, 2),
          (2, C, 3),
          (3, C, 3)])
 
@@ -23,13 +23,13 @@ fsa3 :: Automaton Int SegmentCV
 fsa3 =  ( 1, 
          [4],
          [(1, V, 2),
+         (1, C, 4),
          (2, V, 1),
          (2, C, 3),
          (3, C, 2),
          (3, V, 4),
-         (4, V, 3),
-         (1, C, 4),
-         (4, C, 1)])
+         (4, C, 1),
+         (4, V, 3)])
 
 fsa4 :: Automaton Int SegmentCV
 fsa4 = (1,
@@ -45,15 +45,15 @@ fsa4 = (1,
 fsa5 :: Automaton Int SegmentPKIU
 fsa5 = (1,
        [1, 2, 3],
-       [(1, P, 1),
+       [(1, I, 2),
+       (1, P, 1),
        (1, K, 1),
+       (1, U, 3),
        (1, WB, 1),
-       (1, I, 2),
        (2, I, 2),
        (2, P, 2),
        (2, K, 2),
        (2, WB, 1),
-       (1, U, 3),
        (3, U, 3),
        (3, P, 3),
        (3, K, 3),
@@ -63,20 +63,26 @@ fsa6 :: Automaton Int SegmentPKIU
 fsa6 = (1,
        [1, 2, 3],
        [(1, P, 1),
-       (1, K, 1),
-       (1, I, 1),
-       (1, P, 2),
-       (2, P, 2),
-       (2, K, 2),
-       (2, I, 2),
-       (2, U, 3),
-       (3, P, 3),
-       (3, K, 3),
-       (3, I, 3),
-       (3, U, 3)])
+        (1, P, 2),
+        (1, I, 1),
+        (1, K, 1),
+        (2, P, 2),
+        (2, K, 2),
+        (2, I, 2),
+        (2, U, 3),
+        (3, P, 3),
+        (3, K, 3),
+        (3, I, 3),
+        (3, U, 3)])
 
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
 
 slgToFSA :: SLG sy -> Automaton (ConstructedState sy) sy
-slgToFSA = undefined
+slgToFSA slg =
+  let (starting_symbol, ending_symbol, transitions) = slg in
+  let terminate = [StateForSymbol c | c <- ending_symbol] in
+  let trans = [(ExtraState, c, StateForSymbol c) | c <- starting_symbol] ++
+              [(StateForSymbol x, y, StateForSymbol y) | (x, y) <- transitions]
+  in
+  (ExtraState, terminate, trans)

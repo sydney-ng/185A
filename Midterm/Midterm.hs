330181cdef2d85391d589e8cc70ed81272a4c295
Sydney <sydney.ng@yahoo.com>
2020-05-12 22:11:18 -0700

    fixed transitions for task2

--- Midterm/Midterm.hs
+++ Midterm/Midterm.hs
@@ -63,7 +63,7 @@ helper ((answer, chance): rest) target = case (answer == target) of True -> chan
 -------------------------------------------------------------------------------
 
 buildProbSLG :: Ord a => Corpus a -> ProbSLG a
-buildProbSLG corpus = ProbSLG ((calculate_start_probability corpus), (calculate_end_probability corpus), (format_trans_pairs (calculate_trans_probability corpus)))
+buildProbSLG corpus = ProbSLG ((calculate_start_probability corpus), (calculate_end_probability corpus), [])--(format_trans_pairs (calculate_trans_probability corpus)))
 
 calculate_start_probability :: Ord a => Corpus a -> [(a, Double)] 
 calculate_start_probability corpus = let frequency_pairs = frequencies (extract_starts corpus) 
@@ -77,11 +77,24 @@ calculate_end_probability corpus = let frequency_pairs = frequencies (extract_en
                                      in 
                                          map (\(symbol, probability) -> (symbol, divide probability total) ) frequency_pairs
 
-calculate_trans_probability :: Ord a => Corpus a -> [[((a, a), Double)]]
-calculate_trans_probability corpus = let frequency_pairs = frequencies (get_trans corpus)
-                                         all_states = allStates (corpus)
-                                         in 
-                                         map (\x -> trans_parent_fx frequency_pairs x) all_states
+calculate_trans_probability :: Ord a => Corpus a -> [(a, a, Double)]
+calculate_trans_probability corpus = let transition_body = get_trans corpus
+                                         poss_transitions = nub (get_trans corpus)
+                                     in map (\x-> buildProbSLG_trans_helper2 transition_body x) poss_transitions
+
+buildProbSLG_trans_helper2 :: Ord a => [(a,a)] -> (a, a) -> (a, a, Double)
+buildProbSLG_trans_helper2 t_body (s1, s2) = let numerator = buildProbSLG_count_numerator t_body s1 s2
+                                                 denominator = buildProbSLG_count_base t_body s1 
+                                             in 
+                                             (s1, s2, (divide numerator denominator))
+
+
+buildProbSLG_count_numerator :: Ord a => [(a, a)] -> a -> a -> Int 
+buildProbSLG_count_numerator all_trans target1 target2 = length (filter (\(x, y) -> ((x == target1 ) && (y == target2))) all_trans)
+
+
+buildProbSLG_count_base :: Ord a => [(a, a)] -> a -> Int 
+buildProbSLG_count_base all_trans target = length (filter (\(x, y) -> (x == target)) all_trans)
 
 flatten_trans_pairs :: [[((a, a), Double)]] -> [((a, a), Double)]
 flatten_trans_pairs corpus = concat corpus 
@@ -147,10 +160,6 @@ get_possProbSLG_trans corpus = let transition_body = all_trans_possProbSLG corpu
                                    poss_transitions = nub_all_trans_possProbSLG corpus 
                                in map (\x-> get_possProbSLG_trans_helper2 transition_body x) poss_transitions
 
---get_possProbSLG_trans_helper1 :: [(String,String)] -> (String, String) -> [(String, String, Double)]
---get_possProbSLG_trans_helper1 t_body pairs = map (\x-> get_possProbSLG_trans_helper2 t_body x) pairs
-
-
 get_possProbSLG_trans_helper2 :: [(String,String)] -> (String, String) -> (String, String, Double)
 get_possProbSLG_trans_helper2 t_body (s1, s2) = let numerator = posProbSLG_count_numerator t_body s1 s2
                                                     denominator = posProbSLG_count_base t_body s1 
@@ -171,17 +180,6 @@ posProbSLG_count_base all_trans target = length (filter (\(x, y) -> (x == target
 
 posProbSLG_count_numerator :: [(String, String)] -> String -> String -> Int 
 posProbSLG_count_numerator all_trans target1 target2 = length (filter (\(x, y) -> ((x == target1 ) && (y == target2))) all_trans)
-
---posProbSLG_trans_base all_trans target = sum (map (\(q',p') -> q') (filter (\((x, y), num_occ) -> (y == target)) all_trans))
-
---posProbSLG_trans_calc :: [((String, String), Int)] -> String -> Int -> [((String, String), Double)] 
---posProbSLG_trans_calc [] target total = []
---posProbSLG_trans_calc (((x, xs), chance) : rest) target total = case (xs == target) of True -> ((x, xs), (divide chance total)) : posProbSLG_trans_calc rest target total
---                                                                                       False -> posProbSLG_trans_calc rest target total
-                                        
---trans_parent_fx :: [((String, String), Int)] -> String ->  [((String, String), Double)]
---trans_parent_fx t_freqs symbol = let trans_total = trans_base t_freqs symbol 
---                                 in trans_calc t_freqs symbol trans_total
 
 get_trans_possProbSLG_helper :: Corpus TaggedWord -> [(TaggedWord,TaggedWord)]
 get_trans_possProbSLG_helper [] = []

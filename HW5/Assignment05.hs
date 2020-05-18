42d071df3fabd64cf967b91831dfd9b8e5faccb6
Sydney <sydney.ng@yahoo.com>
2020-05-18 13:55:38 -0700

    finished union

--- HW5/Assignment05.hs
+++ HW5/Assignment05.hs
@@ -2,81 +2,69 @@ module Assignment05 where
 
 import FSA
 
--------------------------------------------------------------------------------
--------------------------------------------------------------------------------
--- IMPORTANT: Please do not change anything above here.
---            Write all your code below this line.
--------------------------------------------------------------------------------
--------------------------------------------------------------------------------
+unionFSAs :: (Ord sy) => 
+              EpsAutomaton Int sy -> 
+              EpsAutomaton Int sy -> 
+              EpsAutomaton Int sy
 
---fwdProb :: (Ord st, Ord sy) => ProbFSA st sy -> [sy] -> st -> Double
---fwdProb pfsa (l_head : l_tail) ending_sym = let end_chance = finProb pfsa2 ending_sym
---                                                start_chance = initProb pfsa2 l_head
---                                            in start_chance * end_chance * (fwdProb_pre_formatter pfsa (l_head : l_tail) ending_sym)  
+unionFSAs automaton fsaB = 
+    let startA = get_starting_symbols automaton
+        finalA = get_ending_symbols automaton
+        transitionA = get_transitions automaton
+        newB_FSA = ensureUnused (allStatesEFSA automaton) fsaB in
+        union_preparser newB_FSA startA finalA transitionA
 
---fwdProb_pre_formatter :: (Ord st, Ord sy) => ProbFSA st sy -> [sy] -> st -> Double
---fwdProb_pre_formatter (starts : ends : trans) (l_head: l_next: l_tail) ending_sym = let curr_trans = l_head 
---                                                                                    curr_state = l_next 
---                                                                                    in trProb pfsa 
+--        unionFSAs_HELPER startA finalA startB finalB transitionA transitionB
+
+union_preparser :: (Ord sy) => 
+                   EpsAutomaton Int sy -> --newB_FSA
+                   Int -> -- startA
+                   [Int] -> -- finalA
+                   [(Int, Maybe sy, Int)] -> -- transitionA 
+                   EpsAutomaton Int sy -- result 
+union_preparser new_fsaB startA finalA transitionA = 
+    let startB = get_starting_symbols new_fsaB
+        finalB = get_ending_symbols new_fsaB
+        transitionB = get_transitions new_fsaB in
+        union_format_trans new_fsaB startA finalA startB finalB transitionA transitionB
+
+union_format_trans ::  (Ord sy) => 
+                       EpsAutomaton Int sy -> --newB_FSA
+                       Int -> -- startA
+                       [Int] -> -- finalA
+                       Int -> -- startB
+                       [Int] -> -- finalB
+                       [(Int, Maybe sy, Int)] -> -- transitionA 
+                       [(Int, Maybe sy, Int)] -> -- transitionB
+                       EpsAutomaton Int sy -- result 
+union_format_trans new_fsaB startA finalA startB finalB transitionA transitionB = 
+     let all_trans = format_transitions (format_transitions transitionA transitionB) [(startA, Nothing, startB)] 
+         all_finals = finalA ++ finalB in
+     unionFSAs_HELPER startA all_finals all_trans
+
+
+get_starting_symbols :: (Ord sy) => EpsAutomaton Int sy -> Int
+get_starting_symbols (EpsAutomaton (start, ends, delta)) = start
+
+get_ending_symbols :: (Ord sy) => EpsAutomaton Int sy -> [Int]
+get_ending_symbols (EpsAutomaton (start, ends, delta)) = ends
+
+get_transitions :: (Ord sy) => EpsAutomaton Int sy -> [(Int, Maybe sy, Int)]
+get_transitions (EpsAutomaton (start, ends, delta)) = delta 
+
+format_transitions :: [(Int, Maybe sy, Int)] -> [(Int, Maybe sy, Int)] ->[(Int, Maybe sy, Int)]  
+format_transitions t1 t2 = t1 ++ t2 
+
+unionFSAs_HELPER :: (Ord sy) => 
+                    Int -> -- startA
+                    [Int] -> -- finalA / finalB
+                    [(Int, Maybe sy, Int)] -> -- transitions
+                    EpsAutomaton Int sy -- answer 
+unionFSAs_HELPER startA finals transitions = (EpsAutomaton (startA, finals, transitions))
 
 
 
--- pass in entire pfsa, list of words left to parse, current symbol we are on 
---calc_transitional_prob :: ProbFSA st sy -> sy-> sy -> st
---calc_transitional_prob pfsa_trans curr_sym next_sym = 
 
 
--- input : 
--- calculate for each of the transitions that are possible 
-find_next_transitions :: (Ord st, Ord sy) => [(st, sy, Double, st)] -> sy -> st -> [(st, sy, Double, st)] 
-find_next_transitions trans_list transition next_st =  (filter (\(_, str, _ ,_) -> (str == transition)) trans_list)
--- find_next_transitions trans_list transition next_st = find_next_transition_with_correct_endstate (filter (\(_, str, _ ,_) -> (str == transition)) trans_list) next_st
 
 
--- input : 
-find_next_transition_with_correct_endstate :: (Ord st, Ord sy) => [(st, sy, Double, st)] -> st -> [(st, sy, Double, st)]
-find_next_transition_with_correct_endstate trans_list next_st = filter (\(st1, _ , _ ,_) -> (st1 == next_st)) trans_list
-
-find_next_word_in_list :: [sy] -> sy 
-find_next_word_in_list (x: xs) = x 
-
-extract_probability :: (Ord st, Ord sy) => (st, sy, Double, st) -> Double
-extract_probability transition = case transition of (_, _ , chance ,_) -> chance 
-
-pfsa_transitions :: (Ord st, Ord sy) => ProbFSA st sy -> [(st, sy, Double, st)]
-pfsa_transitions (ProbFSA (starts, ends, trans)) = trans
-                                                        
--- PseudoCode: 
--- for each start, check for all next transitions 
--- append the probability to running counter 
--- stop when it is a final state 
--- Then go to next 
--------------------------------------------------------------------------------
--------------------------------------------------------------------------------
-
-unionFSAs :: (Ord sy)
-          => EpsAutomaton Int sy
-          -> EpsAutomaton Int sy
-          -> EpsAutomaton Int sy
-unionFSAs = undefined
-
-concatFSAs :: (Ord sy)
-            => EpsAutomaton Int sy
-            -> EpsAutomaton Int sy
-            -> EpsAutomaton Int sy
-concatFSAs = undefined
-
-starFSA :: (Ord sy)
-        => EpsAutomaton Int sy
-        -> EpsAutomaton Int sy
-starFSA = undefined
-
-reToFSA :: (Ord sy)
-        => RegEx sy
-        -> EpsAutomaton Int sy
-reToFSA (Lit a) = undefined
-reToFSA (Alt r1 r2) = undefined
-reToFSA (Concat r1 r2) = undefined
-reToFSA (Star r) = undefined
-reToFSA Zero = undefined
-reToFSA One = undefined
